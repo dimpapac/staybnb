81507d72998af31822a2054238f6a358aad2f97d
dimpapac <demispapa@gmail.com>
2020-09-04 05:16:04 +0300

    login modal

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import logo from '../icons/mainlogo.png' 
 import '../css/navbar.css';
+import { Button } from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
 
 
 class NavBar extends Component {
@@ -8,9 +10,25 @@ class NavBar extends Component {
     constructor (props) {
         super()
         this.state = {
+            show: false,
+            setShow: false
         }
+        this.handleClose = this.handleClose.bind(this)
+        this.handleShow = this.handleShow.bind(this)
     } 
 
+    handleClose(){
+        this.setState({ 
+            setShow: false
+        });
+    };
+
+    handleShow(){
+        console.log('RE')
+        this.setState({ 
+            setShow: true
+        });
+    };
 
     render() { 
         return (
@@ -31,10 +49,24 @@ class NavBar extends Component {
                             <i class="fa fa-bars"/>
                         </a>
                         <div className="dropdown-menu dropdown-menu-right" style={{ marginTop : 10}} aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="#">Εγγραφή</a>
+                        <a className="dropdown-item" onClick={this.handleShow}>Σύνδεση</a>
+                        <Modal show={this.state.setShow} onHide={this.handleClose}>
+                            <Modal.Header className= "text-center" closeButton>
+                              <Modal.Title className= "w-100">Σύνδεση</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                            <Modal.Footer>
+                              <Button variant="secondary" onClick={this.handleClose}>
+                                Close
+                              </Button>
+                              <Button variant="primary" onClick={this.handleClose}>
+                                Save Changes
+                              </Button>
+                            </Modal.Footer>
+                        </Modal>
                         <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Something else here</a>
+                        <a className="dropdown-item" href="#">Βοήθεια</a>
                         </div>
                     </li>
                     </ul>
