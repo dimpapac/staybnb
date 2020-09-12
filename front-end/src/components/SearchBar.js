3039a65fe98e8fc72421cf6bfc715b63e782be71
dimpapac <demispapa@gmail.com>
2020-09-12 18:03:44 +0300

    add AutoCompleteLoc and DatePicker

--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -5,6 +5,10 @@ import { Button } from 'react-bootstrap'
 import Modal from 'react-bootstrap/Modal'
 import { withRouter } from 'react-router'
 
+import AutoCompleteLoc from './AutoCompleteLoc'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 class SearchBar extends Component {
 
@@ -12,10 +16,14 @@ class SearchBar extends Component {
         super()
         this.state = {
             show: false,
-            setShow: false
+            setShow: false,
+            startDate: new Date(),
+            location: " "
         }
 
         this.handleSearchButton = this.handleSearchButton.bind(this);
+        this.handleLocation = this.handleLocation.bind(this);
+
 
     } 
 
@@ -24,10 +32,40 @@ class SearchBar extends Component {
     }
 
 
+    handleChange = date => {
+        this.setState({
+            startDate: date
+        });
+    };
+
+    handleLocation =  (val) => {
+        if (val.length > 0) 
+            this.setState({
+                location: val,
+                locError: false
+            })
+        else
+            this.setState({
+                location: val,
+                locError: true
+            })
+    };
+
     render() { 
         return (
             <React.Fragment>
                 <button onClick={() => this.handleSearchButton("/apartments")} >Search</button>
+                <form>
+                    <div className="form-row">
+                        <AutoCompleteLoc value={this.state.location} handleLocation={this.handleLocation} name="location"/>
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={this.handleChange}
+                            dateFormat="dd/MM/yyyy"
+                        />
+                    </div>
+                </form>
+                
             </React.Fragment>
         )
     }
