4a4a15acfc7b833cbfca07cb4bfa744e9d65060b
TheoKant <kantartzistheophilos@gmail.com>
2020-09-17 17:29:34 +0300

    Photo Grid in Ad Preview

--- front-end/src/components/AdPreview.js
+++ front-end/src/components/AdPreview.js
@@ -2,10 +2,9 @@ import React, {Component} from 'react'
 import logo from '../icons/mainlogo.png' 
 import { Button } from 'react-bootstrap'
 import Modal from 'react-bootstrap/Modal'
-import ReactPhotoGrid from 'react-photo-grid'
 
+import PhotoGrid from './PhotoGrid'
 import Gmap from './Gmap'
-import Carousel from './Carousel'
 
 import { adService } from '../services/ad.service'
 
@@ -59,28 +58,13 @@ class AdPreview extends Component {
         return (
             <div>
             { !this.state.loading && (
-                <div>
-                    <h1 class="float-left">{this.state.info.title}</h1>
-                    <div style={{width: "50%",marginLeft:"50%",background : "#E8E8E8" ,borderRadius: "25px",marginTop:"10px"}}>
-                        { this.state.photos.length >= 3 && (
-                        <ReactPhotoGrid
-                            // onImageClick={this.handleImageClick}
-                            data={this.state.photos}
-                        />)}
-                        { this.state.photos.length < 3 && (
-                        <img src={this.state.photos[0]}/>)}
-                    </div>
-                    <button  type="button" class="btn btn-primary" style={{marginLeft:"70%",width:"15%"}} onClick={this.handleShow}>Περισσότερες Φωτογραφίες</button>
-                    <Modal size="lg" show={this.state.setShow} onHide={this.handleClose} >
-                        <Modal.Header  closeButton >
-                        </Modal.Header>
-                        <Modal.Body>
-                        <div class="text-center" style={{width : "100%"}}>
-                            <Carousel ad={this.state.info} height={"30pc"} width={"12pc"}/>
-                        </div>
-                        </Modal.Body>
-                    </Modal>
+                <div class="container">
+                    <h1 class="row" >{this.state.info.title}</h1>
 
+                    <PhotoGrid info ={this.state.info} photos = { this.state.photos } />
+
+                    <h2 style={{marginTop:"10pc"}}>Πληροφορίες Τοποθεσίας</h2>
+                    <p >Παρε μετρο μετα λεωφορειο μετα κανε στροφη αριστερα μετα ξανα δεξεια και εκανες κυκλο τωρα ξανα απο την αρχη και ετσι θα με βρεις χαχα χιχι χοχο</p>
 					<Gmap ads = {[this.state.info]} height={"40vh"} width={"100%"} marginTop={"10px"}/>
  
                 </div>
---
+++ front-end/src/components/PhotoGrid.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react'
+import { Button } from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
+
+import Carousel from './Carousel'
+
+
+
+
+
+class PhotoGrid extends Component {
+
+    constructor (props , context) {
+        super( props , context )
+        this.state = {
+            photos : props.photos,
+            info : props.info
+        }
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    } 
+
+    componentDidMount(){
+    }
+
+
+    handleShow(){
+        this.setState({ 
+            setShow: true
+        });
+    };
+
+    handleClose(){
+        this.setState({ 
+            setShow: false
+        });
+    };
+
+    render() { 
+
+        return (
+            <div>
+            { this.state.photos.length == 1 && (
+                <div class="row">
+                    <img  class="img img-responsive " style={{borderRadius:"25px",width:"100%",height:"60vh"}} src={this.state.photos[0]}/>
+                </div>
+            )}
+
+            { this.state.photos.length == 2 && (
+
+                <div class="row" style={{width:"100%",height:"45vh"}}>
+                    <div  style={{width:"50%"}}>
+                        <img  src={this.state.photos[0]} style={{borderTopLeftRadius:"25px",borderBottomLeftRadius:"25px",width:"100%",height:"100%"}}/>
+                    </div>
+                    <div  style={{width:"50%"}} >
+                        <img  src={this.state.photos[1]} style={{borderTopRightRadius:"25px",borderBottomRightRadius:"25px",width:"100%",height:"100%"}}/>
+                    </div>
+                </div>
+            )}
+
+            { this.state.photos.length == 3 && (
+
+            <div class="row" style={{width:"100%",height:"45vh"}}>
+                <div  style={{width:"60%"}}>
+                    <img  src={this.state.photos[0]} style={{borderTopLeftRadius:"25px",borderBottomLeftRadius:"25px",width:"100%",height:"100%"}}/>
+                </div>
+                <div class="col" style={{width:"40%",height:"45vh"}}>
+                    <div  style={{height:"50%"}} >
+                        <img  src={this.state.photos[1]} style={{borderTopRightRadius:"25px",width:"100%",height:"100%"}}/>
+                    </div>
+                    <div  style={{height:"50%"}} >
+                        <img  src={this.state.photos[2]} style={{borderBottomRightRadius:"25px",width:"100%",height:"100%"}}/>
+                    </div>
+                </div>
+            </div>
+            )}
+
+            <button  type="button" class="btn btn-primary" style={{marginLeft:"30%",width:"40%"}} onClick={this.handleShow}>Περισσότερες Φωτογραφίες</button>
+            <Modal size="lg" show={this.state.setShow} onHide={this.handleClose} >
+                    <Modal.Header  closeButton >
+                    </Modal.Header>
+                    <Modal.Body>
+                    <div class="text-center" style={{width : "100%"}}>
+                        <Carousel ad={this.state.info} height={"30pc"} width={"12pc"}/>
+                    </div>
+                    </Modal.Body>
+            </Modal>
+            </div>
+        )
+    }
+}
+
+export default PhotoGrid;
